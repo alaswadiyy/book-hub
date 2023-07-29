a8485a5b8c87059061bbce47fb43aa9010792500
Abdulmuiz Sharafadeen <abdulmuizsharafadeen@gmail.com>
2023-07-29 18:32:46 +0100

    fetching from API

--- src/components/BookList/Book.jsx
+++ src/components/BookList/Book.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './BookList.css';
 
-const Book = () => {
+const Book = (book) => {
   return (
-    <div>Book</div>
+    <div className='book-item flex flex-column flex-sb'>
+      <div className="book-item-img">
+        <img src= {book.cover_Img} alt="cover" />
+      </div>
+      <div className="book-item-info text-center">
+        <Link to={`/book/${book.id}`} {...book}></Link>
+      </div>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+import { AppProvider } from './context';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import BookList from './components/BookList/BookList';
@@ -11,13 +12,16 @@ import BookDetails from './components/BookDetails/BookDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element = {<Home/>} />
-      <Route path='about' element = {<About/>} />
-      <Route path='book' element = {<BookList/>} />
-      <Route path='/book/:id' element = {<BookDetails/>} />
-    </Routes>
-  </BrowserRouter>
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<Home/>} />
+        <Route path='about' element = {<About/>} />
+        <Route path='book' element = {<BookList/>} />
+        <Route path='/book/:id' element = {<BookDetails/>} />
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
+  
 );
 
